afb5818e9dcf44ca5e21047a79e22620f5d80c2a
Amanda Rangel <a.rangelsr@gmail.com>
2021-05-18 11:45:43 -0300

    Fixing imports and methods calls in comments handlers

--- src/server/handlers/comments/createComment.ts
+++ src/server/handlers/comments/createComment.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { createUUID, getTokenData } from "../../../utils";
 import { Comment } from "../../../entities";
-import * as database from '../../../data/comments'
-import { getPostById, getUserById } from "../../../data";
+import * as database from '../../../data'
+
 
 export const createComment = async (
   req: Request,
@@ -13,8 +13,8 @@ export const createComment = async (
       req.headers.authorization!
     )
 
-    const user = await getUserById(tokenData!.id)
-    const post = await getPostById(req.params.id)
+    const user = await database.getUserById(tokenData!.id)
+    const post = await database.getPostById(req.params.id)
 
     if (!user) return res.status(404).send("User not found")
     if(!post) return  res.status(404).send('Post not found')
